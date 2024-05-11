95977fb0d40c1547b1da35faffee238854c88efa
Chris Clark <chris.clark@coffeeb4code.com>
2024-05-10 21:09:01 -0500

    basic setup

---
+++ electron.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = !app.isPackaged
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../dist/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
